3aad8b7469aee73fc03586651964ea7d5975ae3c
Mark <markeric.eonbots@gmail.com>
2021-04-10 15:38:12 +0800

    scheduling

--- EonBotzLibrary/schedule.cs
+++ EonBotzLibrary/schedule.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Text;
 using MySql.Data.MySqlClient;
@@ -14,9 +15,41 @@ namespace EonBotzLibrary
         MySqlCommand cmd;
 
         public List<string> datafill = new List<string>();
+        public List<string> datafillroom = new List<string>();
+        public List<string> datafillcourse = new List<string>();
+
+        public string timediff { set; get; }
         public string subjcode { set; get; }
           public string subjTitle { set; get; }
+        public string roomdesc { set; get; }
+        public string date { set; get; }
+        public string timeStart{ set; get; }
+        public string timeEnd { set; get; }
+        public string maxStudent { set; get; }
+        public string status { set; get; }
+        public string roomid { set; get; }
+        public string course { set; get; }
 
+
+        public void times()
+        {
+
+            conn = connect.getcon();
+            conn.Open();
+
+
+
+            cmd = new MySqlCommand("select timediff (timestart,timeend) from schedule where date ='"+date +"'and timestart ='"+timeStart+"'or timeend='"+"'", conn);
+            {
+                mdr = cmd.ExecuteReader();
+
+                while (mdr.Read())
+                {
+                    timediff = mdr[0].ToString();
+                }
+                conn.Close();
+            }
+        }
         public void Schedule()
         {
             conn = connect.getcon();
@@ -24,16 +57,95 @@ namespace EonBotzLibrary
 
             datafill.Clear();
 
-            using(cmd = new MySqlCommand("SELECT subjectcode FROM subjects", conn))
+            using (cmd = new MySqlCommand("SELECT subjectcode FROM subjects", conn))
             {
                 mdr = cmd.ExecuteReader();
 
                 while (mdr.Read())
                 {
-                    datafill.Add(mdr[0].ToString());      
+                    datafill.Add(mdr[0].ToString());
                 }
                 conn.Close();
             }
+
+            conn = connect.getcon();
+            conn.Open();
+
+            datafillroom.Clear();
+
+            using (cmd = new MySqlCommand("SELECT description FROM rooms", conn))
+            {
+                mdr = cmd.ExecuteReader();
+
+                while (mdr.Read())
+                {
+                    datafillroom.Add(mdr[0].ToString());
+                }
+                conn.Close();
+            }
+
+
+            conn = connect.getcon();
+            conn.Open();
+
+            datafillcourse.Clear();
+
+            using (cmd = new MySqlCommand("SELECT description FROM course", conn))
+            {
+                mdr = cmd.ExecuteReader();
+
+                while (mdr.Read())
+                {
+                    datafillcourse.Add(mdr[0].ToString());
+                }
+                conn.Close();
+            }
+
+        }
+        public void insertSched()
+        {
+            status = "available";
+
+            conn = connect.getcon();
+            conn.Open();
+            cmd = new MySqlCommand("select roomID from rooms where description = '" + roomdesc + "'", conn);
+            mdr = cmd.ExecuteReader();
+            while (mdr.Read())
+            {
+                roomid = mdr[0].ToString();
+            }
+            conn.Close();
+
+
+            conn = connect.getcon();
+            conn.Open();
+            cmd = new MySqlCommand("select courseID from course where description = '" + course + "'", conn);
+            mdr = cmd.ExecuteReader();
+            while (mdr.Read())
+            {
+                course = mdr[0].ToString();
+            }
+            conn.Close();
+
+            conn = connect.getcon();
+            conn.Open();
+
+            using (cmd = new MySqlCommand("INSERT INTO schedule(subjectCode,roomid,date,timestart,timeend,status,maxStudent,courseID)VALUES(" +
+                "@subjcode,@roomid,@date,@timestart,@timeend,@status,@maxStudent,@course)", conn))
+            {
+                cmd.Parameters.AddWithValue("@subjcode", subjcode);
+                cmd.Parameters.AddWithValue("@roomid", roomid);
+                cmd.Parameters.AddWithValue("@course", course);
+                cmd.Parameters.AddWithValue("@date", date);
+
+                cmd.Parameters.AddWithValue("@timestart", timeStart);
+                cmd.Parameters.AddWithValue("@timeend", timeEnd);
+                cmd.Parameters.AddWithValue("@status", status);
+                cmd.Parameters.AddWithValue("@maxStudent", maxStudent);
+                cmd.ExecuteNonQuery();
+
+            }
+            conn.Close();
         }
         public void Viewdescription()
         {
@@ -51,6 +163,33 @@ namespace EonBotzLibrary
                 mdr.Close();
                 conn.Close();
             }
+        }
+        public void viewRoomid()
+        {
+            //conn = connect.getcon();
+            //conn.Open();
+
+            //cmd = new MySqlCommand("select roomID from rooms where description = '" + roomdesc + "'", conn);
+            //mdr = cmd.ExecuteReader();
+            //while (mdr.Read())
+            //{
+            //    roomid = mdr[0].ToString(); 
+
+            //}
+            //conn = connect.getcon();
+            //conn.Open();
+            //using (cmd = new MySqlCommand("select roomID from rooms where description =@descrip", conn))
+            //{
+            //    cmd.Parameters.AddWithValue("@descrip", roomdesc);
+            //    mdr = cmd.ExecuteReader();
+            //    mdr.Read();
+            //    if (mdr.HasRows)
+            //    {
+            //        roomid = mdr[0].ToString();
+            //    }
+            //    mdr.Close();
+            //    conn.Close();
+            //}
         }
     }
 }
