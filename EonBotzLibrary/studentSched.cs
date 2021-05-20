85f2ed1f7c8e0abcf2844574536f22e8dc9b4bbd
Mark <markeric.eonbots@gmail.com>
2021-05-20 13:22:15 +0800

    5/20/2021

--- EonBotzLibrary/studentSched.cs
+++ EonBotzLibrary/studentSched.cs
@@ -26,7 +26,7 @@ namespace EonBotzLibrary
             conn.Open();
 
             dt.Clear();
-            using (cmd = new MySqlCommand("SELECT a.schedId, a.subjectCode,e.subjectTitle,d.name,a.timestart,a.timeend,a.date,a.maxStudent,a.status ,e.lab,e.lec FROm subjects e, schedule a ,tuition b,tuitioncategory c,rooms d where a.subjectcode = b.subjectcode and b.tuitioncatid = c.tuitionCatID and d.roomid = a.roomid and e.subjectcode = b.subjectcode and c.category ='" + category+ "'group by a.schedId", conn))
+            using (cmd = new MySqlCommand("select b.schedid,b.subjectcode, c.subjectTitle,d.name,b.date, b.timestart,b.timeend,b.maxstudent,b.status, c.lab,c.lec  from tuition a,schedule b, subjects c,rooms d   where b.subjectcode = c.subjectcode and a.schedid = b.schedid and a.tuitioncatid= '" + category + "' group by a.tuitionid", conn))
             {
                 mdr = cmd.ExecuteReader();
 
